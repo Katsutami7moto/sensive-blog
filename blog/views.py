7e3c84f5df61aabf0fa0863640ea84ae6368f1c3
Katsutami7moto <nclsf87@gmail.com>
2022-08-29 17:16:46 +0300

    Request sorted data from DB with "order_by"

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@ from django.db.models import Count
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -28,24 +24,17 @@ def serialize_tag(tag):
     }
 
 
-def get_likes_count(post):
-    return post.likes__count
-
-
 def index(request):
-
-    most_popular_posts = sorted(
-        Post.objects.annotate(Count('likes')),
-        key=get_likes_count,
-        reverse=True
-    )[:5]
+    most_popular_posts = Post.objects.annotate(
+        Count('likes')
+    ).order_by('-likes__count')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        Count('posts')
+    ).order_by('-posts__count')[:5]
 
     context = {
         'most_popular_posts': [
@@ -84,9 +73,9 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        Count('posts')
+    ).order_by('-posts__count')[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -103,9 +92,9 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        Count('posts')
+    ).order_by('-posts__count')[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
